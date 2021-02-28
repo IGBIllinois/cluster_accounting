52dbd11cd13b91b073cdc6407e20306229ecae0b
root <root@biocluster.igb.illinois.edu>
2021-02-27 19:32:15 -0600

    Version 1.50

--- conf/app.inc.php
+++ conf/app.inc.php
@@ -2,7 +2,7 @@
 /*
 Stores application variables
 */
-define("__VERSION__","1.49");
+define("__VERSION__","1.50");
 define("__WEBSITE_URL__","https://github.com/IGBIllinois/cluster_accounting");
 
 
