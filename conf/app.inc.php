16841931e4c42978ba5c4364b35489c81694000f
David Slater <dslater@illinois.edu>
2023-03-08 12:35:49 -0600

    Version 2.0

--- conf/app.inc.php
+++ conf/app.inc.php
@@ -2,7 +2,7 @@
 /*
 Stores application variables
 */
-define("VERSION","2.0 Beta");
+define("VERSION","2.0");
 define("TWIG_DIR","templates");
 define("EMAIL_CSS","vendor/twbs/bootstrap/dist/css/bootstrap.min.css");
 define("WEBSITE_URL","https://github.com/IGBIllinois/cluster_accounting");
